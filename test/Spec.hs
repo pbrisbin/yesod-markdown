01884f918b7ecc54caf3f5fa012dd05eab965ff9
patrick brisbin <pbrisbin@gmail.com>
2021-09-28 09:42:28 -0400

    Make sanitizing spec less brittle
    Different versions of pandoc and/or xss-sanitize can result in slightly
    different output (quoting, tag or tag-and-contents stripped), so making
    this into more or a property that should hold across all behaviors is
    better.

--- test/Spec.hs
+++ test/Spec.hs
@@ -30,20 +30,17 @@ spec = describe "Yesod.Markdown" $ do
         let Right html = markdownToHtml markdown
 
         let rendered = renderHtml html
-        rendered `shouldBe` TL.concat
-            [ "<h1 id=\"title\">Title</h1>"
-            , "<ul>"
-            , "<li>one</li>"
-            , "<li>two</li>"
-            , "<li>three</li>"
-            , "</ul>"
-            -- pandoc-2.2.3 changes quote escaping in a minor way that we don't
-            -- care about. CPP is annoying, so let's just make the assertion
-            -- dynamic
-            , if "&#39;" `TL.isInfixOf` rendered
-                then "\n  alert(&#39;xxs&#39;);\n"
-                else "\n  alert('xxs');\n"
-            ]
+            expectedPrefix = mconcat
+                [ "<h1 id=\"title\">Title</h1>"
+                , "<ul>"
+                , "<li>one</li>"
+                , "<li>two</li>"
+                , "<li>three</li>"
+                , "</ul>"
+                ]
+
+        rendered `shouldSatisfy` (expectedPrefix `TL.isPrefixOf`)
+        rendered `shouldSatisfy` (not . ("<script>" `TL.isInfixOf`))
 
     it "converts Markdown to unsanitized HTML" $ do
         let
